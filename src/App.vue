ca9885f2ed10c78812d67e6bfd64210ca048c451
Briled <khaledtbk@gmail.com>
2022-08-13 14:05:45 +0100

    Uploading Form

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+<div id="wrapper">
+<form @submit.prevent="submit" >
+  <my-input name="Username"
+  :rules="{required:true,min:5}"
+  :value="username.value"
+  :error="username.error"
+    @update="update"></my-input>
+      <my-input name="Password"
+  :rules="{required:true,min:10}"
+  :value="password.value"
+  :error="password.error"
+  type="password"
+    @update="update"></my-input>
+  <my-button background="darkslateblue" color="white" :disabled="!valid" />
+</form>
+</div>
+</template>
+
+<script>
+import MyButton from '../MyButton.vue';
+import MyInput from '../MyInput.vue';
+export default {
+
+components: {
+  MyButton,
+  MyInput
+},
+data() {
+  return {
+   
+    username : {
+      value: '',
+      error : ''
+    },
+    password : {
+      value: '',
+      error : ''
+  }
+}
+},
+computed: {
+  valid() {return !this.username.error && !this.password.error}
+},
+methods : {
+  update({name , value, error}) {
+    this[name].value = value,
+    this[name].error = error
+  },
+  submit() {
+    console.log('Event')
+  }
+}
+
+}
+</script>
+
+<style scoped>
+#wrapper {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+</style>
