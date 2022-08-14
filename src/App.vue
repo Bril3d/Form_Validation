371c87e23175439a8f7973208b52ddbf3f5e6596
Khaled Briki <81531221+Bril3d@users.noreply.github.com>
2022-08-14 18:31:13 +0100

    adding placeholder

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,15 @@
   :rules="{required:true,min:5}"
   :value="username.value"
   :error="username.error"
-    @update="update"></my-input>
+    @update="update"
+    placeholder="Type your username"></my-input>
       <my-input name="Password"
   :rules="{required:true,min:10}"
   :value="password.value"
   :error="password.error"
   type="password"
-    @update="update"></my-input>
+    @update="update"
+    placeholder="Type your password"></my-input>
   <my-button background="darkslateblue" color="white" :disabled="!valid" />
 </form>
 </div>
